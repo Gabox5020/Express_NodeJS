06804c85bc6f7fcc8ddd750ad9c02030f68cc2c4
Gabriel <gaboxroll.2@gmail.com>
2021-12-30 13:04:38 -0300

    Primer Ejemplo

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-// const http = require('http');
-
-
-// const server = http.createServer((req, res) => {
-//     res.status = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Hello World');
-// });
-
-// server.listen(3000, () => {
-//     console.log('Serve on port 3000');
-// });
-
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.get('/about', (req, res)=> {
+    res.send('About me');
+});
+
+app.get('/contact', (req, res)=> {
+    res.send('Form contact');
 });
 
 app.listen(3000, () => {
