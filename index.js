202f3a9fa70e574c155f601f5c97efa2a0e3826e
Gabriel <gaboxroll.2@gmail.com>
2021-12-30 17:05:51 -0300

    Segundo Ejemplo

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.use(express.json());
+
+app.get('/user', (req, res) => {
+    res.json({
+        username: 'Cameron',
+        lastname: 'Howe'
+    });
 });
 
-app.get('/about', (req, res)=> {
-    res.send('About me');
+app.post('/user/:id', (req, res)=> {
+    console.log(req.body);
+    console.log(req.params)
+    res.send('POST REQUEST RECEIVED');
 });
 
-app.get('/contact', (req, res)=> {
-    res.send('Form contact');
+app.put('/contact', (req, res)=> {
+    res.send('UPDATE REQUEST RECEIVED');
 });
 
-app.listen(3000, () => {
-    console.log('server on port 3000');
+app.delete('/test', (req, res )=> {
+    res.send('<h1>DELETE REQUEST RECEIVED</h1>');
+});
+
+app.listen(5000, () => {
+    console.log('server on port 5000');
 });
