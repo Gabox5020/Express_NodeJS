e342f6d971866b561517bef1c25f5514a53aff40
Gabriel <gaboxroll.2@gmail.com>
2021-12-31 01:23:00 -0300

    Sexto Ejemplo

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-function logger(req, res, next) {
-    console.log(`Route Received: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    next();
-}
 
 app.use(express.json());
-app.use(logger);
+app.use(morgan('dev'));
 
 // app.all('/user', (req, res, next) => {
 //     console.log('Por aqui paso');
